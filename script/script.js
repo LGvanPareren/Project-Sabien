8e6d14c8d9f1416f3821922615f901f32628e9d7
LGvanPareren <320373@student.mboutrecht.nl>
2021-11-19 15:46:55 +0100

    Java has been add for responsive site

---
+++ script/script.js
@@ -0,0 +1,43 @@
+const topMenu = $("nav ul li");
+const topMenuHeight = topMenu.outerHeight() + 15;
+const menuItems = topMenu.find("a");
+const scrollItems = menuItems.map(function () {
+    const item = $($(this).attr("href"));
+    if (item.length) {
+        return item;
+    }
+});
+
+check();
+
+function check() {
+    $(window).scroll(function () {
+        const fromTop = $(this).scrollTop() + topMenuHeight + $('header')[0].offsetHeight;
+
+        let cur = scrollItems.map(function () {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+
+
+        cur = cur[cur.length - 1];
+        const id = cur && cur.length ? cur[0].id : "";
+
+        for (let i = 0; i < menuItems.length; i++) {
+            let menuItem = menuItems[i];
+            if (menuItem.attributes.href.value.replace("#", "") === id)
+                menuItem.classList.add("active");
+            else if (menuItem.classList.contains("active"))
+                menuItem.classList.remove("active")
+        }
+
+    });
+}
+
+$('nav ul').click(function () {
+    if ($('header').hasClass("is-active")) {
+        menu_toggle();
+    }
+})
+
+window.onscroll = check;
