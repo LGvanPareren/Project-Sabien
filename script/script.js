a65b6595718e4b5f368a05d07b8042ed8ff5bf69
LGvanPareren <320373@student.mboutrecht.nl>
2021-11-21 12:55:39 +0100

    all html/css/script add

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
+// navbar is
 const topMenu = $("nav ul li");
 const topMenuHeight = topMenu.outerHeight() + 15;
 const menuItems = topMenu.find("a");
-const scrollItems = menuItems.map(function () {
+const scrollItems = menuItems.map(function() {
     const item = $($(this).attr("href"));
     if (item.length) {
         return item;
@@ -11,10 +12,10 @@ const scrollItems = menuItems.map(function () {
 check();
 
 function check() {
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         const fromTop = $(this).scrollTop() + topMenuHeight + $('header')[0].offsetHeight;
 
-        let cur = scrollItems.map(function () {
+        let cur = scrollItems.map(function() {
             if ($(this).offset().top < fromTop)
                 return this;
         });
@@ -34,10 +35,32 @@ function check() {
     });
 }
 
-$('nav ul').click(function () {
+$('nav ul').click(function() {
     if ($('header').hasClass("is-active")) {
         menu_toggle();
     }
 })
 
 window.onscroll = check;
+
+//caroussel
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
